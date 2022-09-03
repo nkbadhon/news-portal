6a20de54aba501505a371b582e5be38f3a496506
nkbadhon <badhon7574@gmail.com>
2022-09-03 22:44:13 +0600

    news catagories added

--- js/Script.js
+++ js/Script.js
@@ -1,1 +1,18 @@
-console.log('hi')
+fetch('https://openapi.programming-hero.com/api/news/categories')
+    .then(res => res.json())
+    .then(data => display(data))
+
+function display(data) {
+    const xyz = data.data.news_category;
+    const newsCatagoriesContainer = document.getElementById('news-catagories')
+
+    for (datas of xyz) {
+        const li = document.createElement('li');
+        li.innerText = datas.category_name;
+        newsCatagoriesContainer.appendChild(li);
+        // console.log(datas.category_name)
+    }
+    // console.log(data.data.news_category[0])
+}
+
+display();
