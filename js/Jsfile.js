b85e22301dfb5b64fdd897518ab02ec4c690d2f5
nkbadhon <badhon7574@gmail.com>
2022-09-04 12:42:34 +0600

    Caragory id done

--- js/Jsfile.js
+++ js/Jsfile.js
@@ -6,27 +6,42 @@ function newsPageGet() {
 }
 
 function display(data) {
-    console.log(data)
+    // console.log(data.data.news_category)
     const xyz = data.data.news_category;
     const newsCatagoriesContainer = document.getElementById('news-catagories');
-
 
     for (const datas of xyz) {
         idOfEachNews.push(datas.category_id);
         const li = document.createElement('li');
-        li.innerText = datas.category_name;
+
+        li.innerHTML = `
+        <p id="${datas.category_id}" onclick="catagoriesClicked(${datas.category_id})">${datas.category_name}</p>
+        `
         newsCatagoriesContainer.appendChild(li);
 
     }
+
+
+
+    // for (const datas of xyz) {
+    //     idOfEachNews.push(datas.category_id);
+    //     const li = document.createElement('li');
+    //     li.innerText = datas.category_name;
+    //     newsCatagoriesContainer.appendChild(li);
+
+    // }
 
 }
 
 newsPageGet();
 
-document.getElementById('news-catagories').addEventListener('clicked', catagoriesClicked());
+// document.getElementById('news-catagories').addEventListener('clicked', catagoriesClicked());
 
-function catagoriesClicked() {
-
+function catagoriesClicked(id) {
+    let countryId = "(`0${id}`)";
+    fetch(`https://openapi.programming-hero.com/api/news/category/${countryId}`)
+        .then(res => res.json())
+        .then(data => console.log(data))
 }
 
 
