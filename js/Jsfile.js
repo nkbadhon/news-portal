62aa0afc1491940b6b7633d7da8a7f71b820bac6
nkbadhon <badhon7574@gmail.com>
2022-09-04 18:38:47 +0600

    News sho plus modal done

--- js/Jsfile.js
+++ js/Jsfile.js
@@ -34,30 +34,36 @@ function displayNews(abc) {
     const newsShowContainer = document.getElementById('News-Div');
     newsShowContainer.innerHTML = "";
     for (const abcd of abc) {
-        console.log(abcd.title)
+        // console.log(abcd)
         const newsPush = document.createElement('div');
         newsPush.innerHTML = `
-        <div class=" border rounded-3 bg-light d-sm-flex  container my-3 h-25 ">
-        <div><img src="${abcd.image_url}" class="img-fluid" alt="">
+        <div class=" border rounded-3 bg-light d-sm-flex  container my-3" >
+        <div><img src="${abcd.image_url}" class=" my-3" alt=""  style="height: 220px; width: 340px;">
         </div>
         <div class="pt-3 ps-4">
             <h4>${abcd.title}</h4>
-            <p>${abcd.details}
+            <p>${abcd.details.slice(0, 400)}...
             </p>
             <div>
                 <div class="d-flex justify-content-between">
                     <div class="d-flex">
-                        <div class="w-25  h-25 m-3"> <img src="${abcd.author.img}" class="w-100 rounded-circle  h-100" alt=""></div>
+                        <div class=" m-3" style="height: 40px; width:40px; "> <img src="${abcd.author.img}" class="w-100 rounded-circle  h-100" alt=""></div>
                         <div class=" m-3">
-                            <h6>${abcd.author.name}
+                            <h6>Author: ${abcd.author.name ? abcd.author.name : "Author name is not available"}
                             </h6>
-                            <p>${abcd.author.published_date}
+                            <p> Date of Publication: ${abcd.author.published_date ? abcd.author.published_date : "Publish date is not available"}
                             </p>
                         </div>
                     </div>
                     <div class="d-flex m-3 ">
                     <i class="fa-solid fa-eye p-2"></i>
-                        <h3 class="">${abcd.total_abcd}</h3>
+                        <h3 class="">${abcd.total_view ? abcd.total_view : "Total View Is Not Available"}</h3>
+                    </div>
+                    
+                    <div class=" m-3">
+                    <button onclick="loadFullNews('${abcd._id}')" type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#newsDetailsModal" >
+                    Full News
+                    </button>
                     </div>
                 </div>
             </div>
@@ -69,9 +75,57 @@ function displayNews(abc) {
 
 }
 
-
-
-
 // allNews();
 
 newsPageGet();
+
+// full news loader function
+function loadFullNews(id) {
+    const newsMoreUrl = `https://openapi.programming-hero.com/api/news/${id}`;
+    fetch(newsMoreUrl)
+        .then(res => res.json())
+        .then(news => newsToModal(news.data))
+
+
+    console.log(id)
+}
+
+function newsToModal(modals) {
+    const modalTitleContainer = document.getElementById('modalTitel');
+    modalTitleContainer.innerText = "";
+    for (const modal of modals) {
+        const modTitle = document.createElement('div');
+        modTitle.innerHTML = `
+        <div class=" border rounded-3 bg-light   container my-3" >
+        <div><img src="${modal.image_url}" class=" my-3" alt=""  style="height: 220px; width: 340px;">
+        </div>
+        <div class="pt-3 ps-4">
+            <h4>${modal.title}</h4>
+            <p>${modal.details}
+            </p>
+            <div>
+                <div class="d-flex justify-content-between">
+                    <div class="d-flex">
+                        <div class=" m-3" style="height: 40px; width:40px; "> <img src="${modal.author.img}" class="w-100 rounded-circle  h-100" alt=""></div>
+                        <div class=" m-3">
+                            <h6>${modal.author.name ? modal.author.name : "Author info not available"}
+                            </h6>
+                            <p>${modal.author.published_date ? modal.author.published_date : 'Publish Date not available'}
+                            </p>
+                        </div>
+                    </div>
+                    <div class="d-flex m-3 ">
+                    <i class="fa-solid fa-eye p-2"></i>
+                        <h3 class="">${modal.total_view ? modal.total_view : 'view data is not available'}</h3>
+                    </div>
+                </div>
+                <div>
+                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+        `
+        modalTitleContainer.appendChild(modTitle);
+    }
+}
